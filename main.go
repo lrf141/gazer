71e28bc3719f9dc76aea0bf3d80b4730152678ca
lrf141 <lucky.rabbitfoot141@gmail.com>
2021-10-29 13:50:27 +0000

    add urfave cli v2

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
 
 func main() {
-        fmt.Println("hello,world")
+	app := cli.App{
+		Name: "gazer",
+		Action: func(context *cli.Context) error {
+			fmt.Println("call this command")
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
