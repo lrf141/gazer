1f71d1c71686b2574ffb9e61125a16cfceb067d6
kento-takeuchi <lucky.rabbitfoot141@gmail.com>
2023-04-08 02:10:54 +0900

    add base cli define

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <boost/program_options.hpp>
+
+#define VERSION "0.0.1"
+#define CMD_CAPTION "gazer"
 
 int main(int argc, char **argv) {
-	return 0;
+    using namespace boost::program_options;
+    options_description optionsDescription(CMD_CAPTION);
+    optionsDescription.add_options()
+            ("help,H", "show help")
+            ("version,v", "show version");
+
+    variables_map variablesMap;
+    basic_parsed_options<char> basicParsedOptions = parse_command_line(argc, argv, optionsDescription);
+    store(basicParsedOptions, variablesMap);
+    notify(variablesMap);
+
+    if (variablesMap.count("help")) {
+        std::cout << optionsDescription << std::endl;
+        return 0;
+    }
+
+    if (variablesMap.count("version")) {
+        std::cout << VERSION << std::endl;
+        return 0;
+    }
+
+    return 0;
 }
